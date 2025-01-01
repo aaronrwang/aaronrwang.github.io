76050b0422025754db771f86cbdd278cab1fd371
Aaron Wang <aaronwang04@icloud.com>
2025-01-01 18:08:40 +0800

    STILL IN PROGRESS BUT WANTED TO ADD SOME OLD STUFF AND NEW WORKING ON STUFF

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -3,24 +3,45 @@ import ccs from '../../assets/projects/ccs.jpeg';
 import gostop from '../../assets/projects/GoStop.jpeg';
 export const projects = [
     {
+        'title': 'Tango Bot',
+        'github': 'https://github.com/Elliotkim2/TangoBot',
+        'img': null,
+        'description': 'Built a webapp that solves the Linkedin Tango and posts detailed solutions each day to teach others how to play this game',
+        'skills': 'Python, sql, docker, Next.js'
+    },
+    {
         'title': 'Go Stop (HwaTu)',
         'github': 'https://github.com/aaronrwang/HwaTu',
         'img': gostop,
-        'description': 'Building an online Web app to allow users to play a traditional Korean card Game online',
+        'description': 'Built an online Web app to allow users to play a traditional Korean card Game online. Through this project, I learned about creating/using APIs and sockets to connect clients and servers through node.js and it\'s socket.io library.',
         'skills': 'Node.js, Express.js, Socket.io, React'
     },
     {
         'title': 'CCS Website',
-        'github': 'https://github.com/ccsofnd/ccswebsite.git',
+        'github': 'https://github.com/ccsofnd/ccswebsite',
         'img': ccs,
-        'description': 'Built a website for the Chinese Cultural Society at Notre Dame.<br/>In progress',
+        'description': 'IN PROGRESS <br/>Built a website for the Chinese Cultural Society at Notre Dame.<br/> Focused on developing skills in React and UI Design.',
         'skills': 'React, HTML, Javascript, CSS'
     },
     {
+        'title': 'Memory',
+        'github': 'https://github.com/aaronrwang/Memory-ios-app',
+        'img': null,
+        'description': 'Developed a memory card matching game in Swift, incorporating responsive animations. The project integrated custom sound effects using AVFoundation.',
+        'skills': 'IOS App dev, Swift, UIKit, AVFoundation'
+    },
+    {
+        'title': 'WarCard',
+        'github': 'https://github.com/aaronrwang/War-ios-app',
+        'img': null,
+        'description': 'Developed a fully functional War card game app in Swift, implementing UI design with UIKit, managing app lifecycle events and random number generation.',
+        'skills': 'IOS App dev, Swift, UIKit'
+    },
+    {
         'title': 'Connect Four',
-        'github': 'https://github.com/aaronrwang/Connect-Four.git',
+        'github': 'https://github.com/aaronrwang/Connect-Four',
         'img': connectfour,
-        'description': 'Built a 2-player Connect Four Game using Java AWT (Swing) GUI Library.',
+        'description': 'Built a 2-player Connect Four Game using Java AWT (Swing) GUI Library. Learned about  object oriented programming fundamentals, and Java GUI libraries.',
         'skills': 'Java AWT & Swing'
     }
 ]
